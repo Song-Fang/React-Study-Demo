df895598b218438abeb81159afd1735cfcd96abb
hitfangsong <sof10@pitt.edu>
2019-08-27 15:04:53 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

---
+++ ant-design-demo/src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Component} from 'react';
+import {Button,Toast} from 'antd-mobile';
+
+class App extends Component {
+
+  handleClick=()=>{
+      Toast.loading('Loading...',2,()=>
+      {alert('It has been loaded!')}
+    );
+  }
+
+  render(){
+    return (
+      <div>
+        <Button onClick={this.handleClick} type="primary">Start</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ant-design-demo/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './app.jsx';
+import 'antd-mobile/dist/antd-mobile.css';
+//import from node modules, so the path is correct
+render(<App/>,document.getElementById('root'));
