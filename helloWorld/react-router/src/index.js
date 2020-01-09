36adeaf1cb4cd92af75bec1e1cff9072ffcec55c
Song-Fang <sof10@pitt.edu>
2020-01-08 23:56:12 -0500

    Signed-off-by: Song-Fang <sof10@pitt.edu>

---
+++ helloWorld/react-router/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './component/app';
+import {BrowserRouter} from 'react-router-dom'
+render(
+  (<BrowserRouter>
+    <App/>
+  </BrowserRouter>),document.getElementById('root'));
